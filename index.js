6a0e0a617f067dbc6bdb68f0172670bf6b49b62f
gdfrgft <isjclyakoloskov@hotmail.com>
2024-05-10 07:57:21 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-function numDistinct(s, t) {
-  const m = s.length;
-  const n = t.length;
-  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-  for (let i = 0; i <= m; i++) {
-    dp[i][0] = 1;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
   }
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (s[i - 1] === t[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-      } else {
-        dp[i][j] = dp[i - 1][j];
-      }
-    }
-  }
-  return dp[m][n];
+  return i === s.length;
 }
