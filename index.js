d2bd6bd864dac021dbf0422350d603ebf0495793
gdfrgft <isjclyakoloskov@hotmail.com>
2024-05-12 23:11:17 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function add(x, y) {
-  return x + y;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
 }
-
-const result = add(5, 3);
-console.log(result); // 8
